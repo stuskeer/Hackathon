0b2497643dc2431b5930f002a2912c6663f0251c
Stuart <sskeer@aol.com>
2025-11-19 11:58:38 +0000

    updayed clean

--- clean.py
+++ clean.py
@@ -4,6 +4,30 @@ from ingest import load_excel_data
 # start with data loading
 edinburgh_df, strathspey_df = load_excel_data()
 
+# drop first 2 header rows
+for sheet_name in edinburgh_df:
+    edinburgh_df[sheet_name] = edinburgh_df[sheet_name].iloc[2:].reset_index(drop=True)
+
+for sheet_name in strathspey_df:
+    strathspey_df[sheet_name] = strathspey_df[sheet_name].iloc[5:].reset_index(drop=True)
+
+# remove empty rows
+for sheet_name in edinburgh_df:
+    edinburgh_df[sheet_name] = edinburgh_df[sheet_name].dropna(how='all').reset_index(drop=True)
+
+for sheet_name in strathspey_df:
+    strathspey_df[sheet_name] = strathspey_df[sheet_name].dropna(how='all').reset_index(drop=True)
+
+# print data heads after dropping headers and emopty rows
+print("Edinburgh Daytime Data after dropping headers and empty rows:")
+for sheet_name in edinburgh_df:
+    print(edinburgh_df[sheet_name].head())
+
+print("\nStrathspey Daytime Data after dropping headers and empty rows:")
+for sheet_name in strathspey_df:
+    print(strathspey_df[sheet_name].head())
+
+'''
 # remove nan values
 for sheet_name in edinburgh_df:
     edinburgh_df[sheet_name] = edinburgh_df[sheet_name].dropna()
@@ -22,3 +46,5 @@ for sheet_name in edinburgh_df:
 print("\nCleaned Strathspey Daytime Data:")
 for sheet_name in strathspey_df:
     print(f"Sheet: {sheet_name}, Shape: {strathspey_df[sheet_name].shape}")
+
+    '''
